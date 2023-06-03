d529563160b372831da562fbcbae7f0d93e1d806
Warut Surapat <s.warut@gmail.com>
2023-06-03 22:15:58 +0900

    Make penguin works periodically.

--- lib/tapii/penguin.ex
+++ lib/tapii/penguin.ex
@@ -2,17 +2,33 @@ defmodule Tapii.Penguin do
   use GenServer
   require Logger
 
+  defstruct [:interval_sec, :items]
+
   # Server
   @impl true
   def init(elements) do
-    initial_state = String.split(elements, ",", trim: true)
-    {:ok, initial_state}
+    items = String.split(elements, ",", trim: true)
+    state = %{interval_sec: 10, items: items}
+
+    state
+    |> schedule_work()
+    |> then(&{:ok, &1})
+  end
+
+  def schedule_work(state) do
+    IO.puts("Penguin is scheduling job in 10 seconds")
+    Process.send_after(self(), :perform_work, :timer.seconds(state.interval_sec))
+    state
+  end
+  def perform_work(state) do
+    IO.puts("Penguin is working!")
+    state
   end
 
   @impl true
   def handle_call(:pop, _from, state) do
-    [to_caller | new_state] = state
-    {:reply, to_caller, new_state}
+    [to_caller | new_state] = state.items
+    {:reply, to_caller, %{state | items: new_state}}
   end
   def handle_call(:show, _from, state) do
     IO.puts("Stack of Penguin")
@@ -28,19 +44,20 @@ defmodule Tapii.Penguin do
   @impl true
   def handle_cast({:push, element}, state) do
     new_state = [element | state]
-    {:noreply, new_state}
+    {:noreply, %{state | items: new_state}}
+  end
+
+  @impl true
+  def handle_info(:perform_work, state) do
+    IO.puts("Penguin's handle_info")
+    state
+    |> schedule_work
+    |> perform_work
+    |> then(&{:noreply, &1})
   end
 
   # Client
   def start_link(default) when is_binary(default) do
     GenServer.start_link(__MODULE__, default, name: Penguin)
-  end
-
-  def push(pid, element) do
-    GenServer.cast(pid, {:push, element})
-  end
-
-  def pop(pid) do
-    GenServer.call(pid, :pop)
   end
 end
