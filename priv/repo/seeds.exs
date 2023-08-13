bec099a70b07dad8cdb0e121a49fa9ba998a70d1
Warut Surapat <s.warut@gmail.com>
2023-08-13 18:16:50 +0900

    Update seed.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,47 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Tapii.Repo.delete_all(Tapii.QueryEngines.QueryTemplateGroup)
+
+{:ok, tpg} = Tapii.QueryEngines.create_query_template_group(%{
+  name: "Tenjin - elearning SLO"
+})
+
+{:ok, tp} = Tapii.QueryEngines.create_query_template(%{
+  name: "Tenjin - role_assignment_elearning queue size",
+  query_template_group_id: tpg.id,
+  query: """
+    "tenjin-production" "[SIDEKIQ_CLOUDWATCH_HELPER]" "role_assignment_elearning"
+    | parse "'role_assignment_elearning': *"" as queue_size
+    | timeslice 1m
+    | max(queue_size) as mq by _timeslice
+    | order by _timeslice desc
+  """
+})
+
+{:ok, tp2} = Tapii.QueryEngines.create_query_template(%{
+  name: "Tenjin - course_mapping_elearning queue size",
+  query_template_group_id: tpg.id,
+  query: """
+    "tenjin-production" "[SIDEKIQ_CLOUDWATCH_HELPER]" "course_mapping_elearning"
+    | parse "'course_mapping_elearning': *"" as queue_size
+    | timeslice 1m
+    | max(queue_size) as mq by _timeslice
+    | order by _timeslice desc
+  """
+})
+
+Tapii.Schedulers.create_scheduler(%{
+  active: true,
+  query_template_id: tp.id,
+  occurence: 1,
+  schedule_time: "19:30:00"
+})
+
+Tapii.Schedulers.create_scheduler(%{
+  active: true,
+  query_template_id: tp2.id,
+  occurence: 1,
+  schedule_time: "19:30:00"
+})
