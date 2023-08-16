361008db003953bc5dc6fae1e0d308fab001657d
Warut Surapat <s.warut@gmail.com>
2023-08-17 07:36:28 +0900

    Fix incorrect token in job retrieval.

--- lib/tapii/search_job_retrieval.ex
+++ lib/tapii/search_job_retrieval.ex
@@ -11,7 +11,7 @@ defmodule Tapii.SearchJobRetrieval do
   def run(job_id) do
     Logger.info("#{__MODULE__} - Retrieving result for job id #{job_id}")
 
-    headers = ["Authorization": "Basic c3Vlc01CZEVvRHNUYnY6bVJyUklxUUJ1MERCd3FPdjNXaXNoZ2VxMEVYNmNjM3BpYnRJdlBkRFpNaHdRN0gzdlVRYlRxUDhWNmJkVG5LbA==", "Content-Type": "application/json"]
+    headers = ["Authorization": "Basic c3UxazRIZno5OExkbFU6VEFrTTE0U1oyVE9jYUlMNDdENFZhV0Fvc1hwRTB1aXNrTmFZcUNNQ3Q5S3BjVDM0Q3lJQkE5QWdIWngwWWgwSQ==", "Content-Type": "application/json"]
     url = "https://api.sumologic.com/api/v1/search/jobs/#{job_id}/records?offset=0&limit=300"
 
     {:ok, resp} = HTTPoison.get(url, headers)
