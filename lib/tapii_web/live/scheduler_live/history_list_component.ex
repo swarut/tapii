0b487998b0225a3377f225cd7cae449883dcbcc9
Warut Surapat <s.warut@gmail.com>
2024-01-12 14:07:09 +0900

    Some clean up.

--- lib/tapii_web/live/scheduler_live/history_list_component.ex
+++ lib/tapii_web/live/scheduler_live/history_list_component.ex
@@ -1,9 +1,5 @@
 defmodule TapiiWeb.SchedulerLive.HistoryListComponent do
   use TapiiWeb, :live_component
-
-  alias Tapii.Schedulers
-  alias Tapii.Pon
-
   require Logger
 
   @impl true
@@ -23,32 +19,6 @@ defmodule TapiiWeb.SchedulerLive.HistoryListComponent do
       </.table>
     </div>
 
-    """
-  end
-
-  def render(assigns) do
-    ~H"""
-    <div>
-      <.header>
-        <%= @title %>
-        <:subtitle>Use this form to manage query_template records in your database.</:subtitle>
-      </.header>
-
-      <.simple_form
-        for={@form}
-        id="query_template-form"
-        phx-target={@myself}
-        phx-change="validate"
-        phx-submit="save"
-      >
-        <.input field={@form[:name]} type="text" label="Name" />
-        <.input field={@form[:query]} type="textarea" cols="40" rows="10" label="Query" />
-        <.input field={@form[:query_template_group_id]} type="text" label="Query Template Group" />
-        <:actions>
-          <.button phx-disable-with="Saving...">Save Query template</.button>
-        </:actions>
-      </.simple_form>
-    </div>
     """
   end
 end
--- lib/tapii_web/live/scheduler_live/trigger_component.ex
+++ lib/tapii_web/live/scheduler_live/trigger_component.ex
@@ -7,10 +7,10 @@ defmodule TapiiWeb.SchedulerLive.TriggerComponent do
 
   require Logger
 
-  @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
+  # @impl true
+  # def mount(_params, _session, socket) do
+  #   {:ok, socket}
+  # end
 
   @impl true
   def render(assigns) do
