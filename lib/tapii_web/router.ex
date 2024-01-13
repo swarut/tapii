f1aec734b1bbc4d471acdccc576c15eee86e5a67
Warut Surapat <s.warut@gmail.com>
2024-01-13 23:28:41 +0900

    Add user support.

--- lib/tapii_web/router.ex
+++ lib/tapii_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TapiiWeb.Router do
   use TapiiWeb, :router
 
+  import TapiiWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule TapiiWeb.Router do
     plug :put_root_layout, {TapiiWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -79,6 +82,44 @@ defmodule TapiiWeb.Router do
 
       live_dashboard "/dashboard", metrics: TapiiWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", TapiiWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{TapiiWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserLive.UserRegistrationLive, :new
+      live "/users/log_in", UserLive.UserLoginLive, :new
+      live "/users/reset_password", UserLive.UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserLive.UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", TapiiWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{TapiiWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserLive.UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserLive.UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", TapiiWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{TapiiWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserLive.UserConfirmationLive, :edit
+      live "/users/confirm", UserLive.UserConfirmationInstructionsLive, :new
     end
   end
 end
